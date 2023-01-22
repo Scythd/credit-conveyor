9710638f39a5483444f77bb19c2638891fa6f976
Никита Мокляк <nikita.dezertir@gmail.com>
2023-01-22 14:20:48 +0400

    deal ms init

---
+++ deal/src/main/java/com/moklyak/conveyor/DealApplication.java
@@ -0,0 +1,13 @@
+package com.moklyak.conveyor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConveyorApplication.class, args);
+    }
+
+}
---
+++ deal/src/test/java/com/moklyak/conveyor/ConveyorApplicationTests.java
@@ -0,0 +1,48 @@
+package com.moklyak.conveyor;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moklyak.conveyor.controllers.ConveyorController;
+import com.moklyak.conveyor.dtos.LoanApplicationRequestDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@SpringBootTest(classes = ConveyorController.class)
+class ConveyorApplicationTests {
+
+    @Autowired
+    ConveyorController controller;
+    protected MockMvc mvc;
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    protected void setUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+    protected String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(obj);
+    }
+    protected <T> T mapFromJson(String json, Class<T> clazz)
+            throws JsonParseException, JsonMappingException, IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, clazz);
+    }
+
+    
+}
